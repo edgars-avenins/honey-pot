0f6828e11c5d1e743be58d1ef69ee19542fe876e
Edgars <edgars.avenins@gmail.com>
2020-04-26 14:57:48 +1200

    heroku error

--- client/apis/api.js
+++ client/apis/api.js
@@ -3,31 +3,37 @@ import { validateUrl } from '../utils'
 
 export const getRobotData = (url) => {
     const goodUrl = validateUrl(url)
+    console.log('send robot request', url);
+    
     return request
-        .post(`/v1/api/`)
-        .send(goodUrl)
-        .then(res => {
-            if(res.body.length == 1){
-                console.log('getRobotData length == 1: ',res.body)
-                return getXMLData({url: res.body[0]})
-            }
-            else if(res.body.length == 0){
-                let {url} = goodUrl
-                console.log(url, goodUrl);
-                
-                url += 'sitemap.xml'
-                return getXMLData({url: url})
-            }
-            return res.body
-        })
-        .catch(err => console.error(err))
+    .post(`/v1/api/`)
+    .send(goodUrl)
+    .then(res => {
+        console.log('get robot request', res);
+        if(res.body.length == 1){
+            return getXMLData({url: res.body[0]})
+        }
+        else if(res.body.length == 0){
+            let {url} = goodUrl
+            console.log(url, goodUrl);
+            
+            url += 'sitemap.xml'
+            return getXMLData({url: url})
+        }
+        return res.body
+    })
+    .catch(err => console.error(err))
 }
 
 export const getXMLData = (url) => {    
+    console.log('send xml request', url);
     return request
-        .post('/v1/api/xml/')
-        .send(url)
-        .then(res => res.body)
+    .post('/v1/api/xml/')
+    .send(url)
+    .then(res => {
+        console.log('get xml request', res);
+        return res.body
+    })
         .catch(err => console.error(err))
 
 }
