7e3ee0a9969f4b7515f1e7971f59f11f78dced22
Edgars <edgars.avenins@gmail.com>
2020-04-26 16:51:01 +1200

    heroku error

--- client/apis/api.js
+++ client/apis/api.js
@@ -9,14 +9,14 @@ export const getRobotData = (url) => {
     .send(goodUrl)
     .then(res => {
         if(res.body.length == 1){
-            getXMLData({url: res.body[0]})
+            return getXMLData({url: res.body[0]})
                 .then(res => res.body)
         }
         else if(res.body.length == 0){
             let {url} = goodUrl
             
             url += 'sitemap.xml'
-            getXMLData({url: url})
+            return getXMLData({url: url})
                 .then(res => res.body)
 
         }else return res.body
