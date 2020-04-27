73cfa7d8c1f78b0ee38ef090f96cf3b15cd87bfd
Edgars <edgars.avenins@gmail.com>
2020-04-27 17:22:43 +1200

    split up components with similar functionality

--- client/components/DelfiToday.jsx
+++ client/components/DelfiToday.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getRobotData, getXMLData } from '../apis/api'
+import { getRobotData, getXMLData } from '../apis/xml'
 import { applySimpleFilter, applyComplexFilter } from '../utils'
 
 class DelfiToday extends React.Component {
