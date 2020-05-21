ca35c78b1208da2a87b9542daf58ba6b80179df2
Edgars <edgars.avenins@gmail.com>
2020-05-22 11:45:12 +1200

    Fix navbar collapse

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,28 +2,47 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-export const Nav = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <a className="navbar-brand" href="#">Home</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+class Nav extends React.Component {
+    constructor(props){
+        super(props)
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to='/xml'>XML</Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to='/stuff'>Stuff</Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to='/delfi'>Delfi</Link>
-                    </li>
-                </ul>
-                
-            </div>
-        </nav>
-    )
+        this.state = {
+            collapsed: true,
+        }
+    }
+
+    toggleNavbar = () => {
+        this.setState({collapsed: !this.state.collapsed})
+    }
+    
+    render() {
+        const collapsed = this.state.collapsed
+        const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+        const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                <a className="navbar-brand" href="#">Home</a>
+                <button className={`${classTwo}`} onClick={this.toggleNavbar} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className={`${classOne}`} onClick={this.toggleNavbar} id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to='/xml'>XML</Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link className="nav-link" to='/stuff'>Stuff</Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link className="nav-link" to='/delfi'>Delfi</Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        )
+    }
 }
+
+export default Nav
